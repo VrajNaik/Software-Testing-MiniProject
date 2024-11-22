5f96cb222730dbb84f7661c78539faab429fda3c
VrajNaik <vrajnaik5421@gmail.com>
2024-11-22 13:19:56 +0530

    EMI+Gratuity Added

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,43 @@
 package org.example;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        try{
+            Scanner scanner = new Scanner(System.in);
+            Integer id = -1;
+            while(true){
+                System.out.println("Which calculator you want to try?");
+                System.out.println("1. EMI\n2. Gratuity\nPress any other key to exit\n");
+                try{
+                    id = scanner.nextInt();
+                }
+                catch(InputMismatchException exception){
+                    return;
+                }
+                if(id>=0 && id<8){
+                    break;
+                }
+                else{
+                    System.out.println("Please enter valid option!");
+                }
+            }
+
+            if(id==0){
+                System.out.println("Thanks!!");
+            }
+            else if(id==1){
+                EMICalculator calc = new EMICalculator();
+                calc.init();
+            }
+            else if(id==2){
+                GratuityCalculator calc = new GratuityCalculator();
+                calc.init();
+            }
+        } catch (Exception exception){
+            return;
+        }
     }
 }
