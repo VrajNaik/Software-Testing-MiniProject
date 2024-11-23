b2f48ef37baab07830641cfa0e56a243a99d435a
ARJUN1220 <arjungangani12@gmail.com>
2024-11-23 21:55:45 +0530

    TacCal

---
+++ src/main/java/org/example/TaxCalculator.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import java.util.*;
+
+public class TaxCalculator {
+    private HashMap<Integer,HashMap<Integer,ArrayList<ArrayList<Integer>>>> slab;
+
+    private Long type;
+
+    private Long regimeId;
+
+    private Scanner scanner;
+
+    private Long ageGroupId;
+
+    private Double deductedAmount;
+
+    private Double taxableAmount;
+
+    private Double taxAmount;
+
+    private Double surCharge;
+
+    private Double netTax;
+
+    private Double healthAndEduCess;
+}
