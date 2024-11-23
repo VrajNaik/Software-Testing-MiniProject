ed0241d7a37ed67d32096efe51709e7f94a72700
ARJUN1220 <arjungangani12@gmail.com>
2024-11-23 22:02:33 +0530

    feature added

--- src/main/java/org/example/TaxCalculator.java
+++ src/main/java/org/example/TaxCalculator.java
@@ -96,4 +96,50 @@ public class TaxCalculator {
     public void setHealthAndEduCess(Double healthAndEduCess) {
         this.healthAndEduCess = healthAndEduCess;
     }
+
+    private void initSlab(){
+
+        // 1--> old regime
+        // 2--> new regime
+
+        slab = new HashMap<>();
+
+        slab.put(1,new HashMap<>());
+        slab.put(2,new HashMap<>());
+
+        // < 60
+        ArrayList<ArrayList<Integer>> less60 = new ArrayList<>();
+        ArrayList<ArrayList<Integer>> a60l80 = new ArrayList<>();
+        ArrayList<ArrayList<Integer>> a80 = new ArrayList<>();
+        ArrayList<ArrayList<Integer>> all = new ArrayList<>();
+
+
+
+        less60.add(new ArrayList<>(Arrays.asList(250000,0)));
+        less60.add(new ArrayList<>(Arrays.asList(250000,5)));
+        less60.add(new ArrayList<>(Arrays.asList(500000,20)));
+        less60.add(new ArrayList<>(Arrays.asList(-1,30)));
+        slab.get(1).put(1,less60);
+
+        a60l80.add(new ArrayList<>(Arrays.asList(300000,0)));
+        a60l80.add(new ArrayList<>(Arrays.asList(200000,5)));
+        a60l80.add(new ArrayList<>(Arrays.asList(500000,20)));
+        a60l80.add(new ArrayList<>(Arrays.asList(-1,30)));
+        slab.get(1).put(2,a60l80);
+
+        a80.add(new ArrayList<>(Arrays.asList(500000,0)));
+        a80.add(new ArrayList<>(Arrays.asList(500000,20)));
+        a80.add(new ArrayList<>(Arrays.asList(-1,30)));
+        slab.get(1).put(3,a80);
+
+        all.add(new ArrayList<>(Arrays.asList(250000,0)));
+        all.add(new ArrayList<>(Arrays.asList(250000,5)));
+        all.add(new ArrayList<>(Arrays.asList(250000,10)));
+        all.add(new ArrayList<>(Arrays.asList(250000,15)));
+        all.add(new ArrayList<>(Arrays.asList(250000,20)));
+        all.add(new ArrayList<>(Arrays.asList(250000,25)));
+        all.add(new ArrayList<>(Arrays.asList(-1,30)));
+        slab.get(2).put(0,all);
+
+    }
 }
